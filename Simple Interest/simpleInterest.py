daa6ba10623577b84d09dc06d8601b864e8a836a
Maulik Padh <maulik.padh93@gmail.com>
2019-08-24 19:15:45 -0500

    Simple Interest calculation

---
+++ Simple Interest/simpleInterest.py
@@ -0,0 +1,18 @@
+"""Simple interest is a quick method of calculating the interest charge on
+a loan. Simple interest is determined by multiplying the daily interest rate
+by the number of days that elapse between payments by the principal
+
+S.I. = (P x R x T)/100
+
+"""
+
+# Python3 program for finding simple interest for a given principal
+# amount, time and rate of interest
+
+P, R, T = (1, 1, 1)
+
+# Calculates Simple Interest
+SI = (P*R*T)/100
+
+# Prints resultant value of SI
+print("Simple Interest is : ", SI)
